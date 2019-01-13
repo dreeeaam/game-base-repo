df97380365839197a70db26f1cf2f5967a290ce0
Ashley <ashleyjworrell@gmail.com>
2019-01-13 23:24:38 +0000

    First Version
    The absolute game base

---
+++ Assets/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float playerSpeed = 5.0f;
+
+	// Update is called once per frame
+	void Update () {
+        // Make a Vector2 of the players current Position
+        Vector2 playerPos = gameObject.transform.position;
+
+        // Check which key is pressed and move in the direction
+        if (Input.GetKey("a")) { playerPos.x -= playerSpeed * Time.deltaTime; }
+        if (Input.GetKey("d")) { playerPos.x += playerSpeed * Time.deltaTime; }
+        if (Input.GetKey("w")) { playerPos.y += playerSpeed * Time.deltaTime; }
+        if (Input.GetKey("s")) { playerPos.y -= playerSpeed * Time.deltaTime; }
+
+        // Update the players position
+        gameObject.transform.position = playerPos;
+    }
+}
